4cb080085a4151071bac07426fdf64053f2077e5
Ben Cross <ben_cross@me.com>
2022-07-15 15:57:06 +0200

    test launch 1

---
+++ script.js
@@ -0,0 +1,18 @@
+// let image1;
+//
+//
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   background(220);
+//   image1 = loadImage('images/mouseimg1.png');
+// }
+//
+// function mouseDragged() {
+//   let number = random( 4, 12);
+//   image( image1, mouseX, mouseY, image1.width / number, image1.height / number);
+// }
+//
+//
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
