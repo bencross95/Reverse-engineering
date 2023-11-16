383dcfe6fb359be500541f5e9f9afc495660269d
Ben Cross <ben_cross@me.com>
2023-11-16 11:39:26 +0100

    added opacity

--- animations.js
+++ animations.js
@@ -1,9 +1,11 @@
-
-
 var tl = gsap.timeline();
+
 // gsap.to(".ui-div-bottom", { y: 10 })
 
 // tl.fromTo(".ui-section", { y: 10 })
 
-
-tl.fromTo(".ui-section", { y: -20 }, { duration: 0.5, y: 0 });
+tl.fromTo(
+  ".ui-section",
+  { y: -20, opacity: 0 },
+  { duration: 0.5, delay: 0.5, y: 0, opacity: 1 }
+);
