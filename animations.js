d42ab0d8d4d47f8b283c24ef7b8b00c3fb9fb8ab
Ben Cross <ben_cross@me.com>
2023-11-16 15:20:12 +0100

    again

--- animations.js
+++ animations.js
@@ -17,9 +17,10 @@ tl.fromTo(
   { duration: 0.3, delay: 0.2, opacity: 1 }
 );
 
-tl.to(".yellow-container", {
+tl.to(".yellow-container, .ui-div-bottom", {
   keyframes: {
     opacity: [0.5, 1, 0.3, 1],
+    background: ["#FFFFFF", "#000002", "#FFFFFF", "#000002"],
     // y: [0, 0, 100, 100, 0],
     // ease: "power1.inOut"
   },
@@ -30,9 +31,10 @@ tl.to(".yellow-container", {
 
 tl.to(".ui-top-lvl2", {
   keyframes: {
-    opacity: [0.7, 1, 0.9, 1],
+    opacity: [0.8, 1, 0.9, 1],
     // y: [0, 0, 100, 100, 0],
     // ease: "power1.inOut"
   },
   duration: 0.2
 });
+
