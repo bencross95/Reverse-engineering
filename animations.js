260af00a8c34c41f7a3c043776577e7ca021ff16
Ben Cross <ben_cross@me.com>
2023-11-16 15:13:14 +0100

    animation 3

--- animations.js
+++ animations.js
@@ -9,3 +9,30 @@ tl.fromTo(
   { y: -20, opacity: 0 },
   { duration: 0.5, delay: 0.5, y: 0, opacity: 1 }
 );
+
+
+tl.fromTo(
+  ".yellow-container",
+  { y: 0, opacity: 0 },
+  { duration: 0.3, delay: 0.2, opacity: 1 }
+);
+
+tl.to(".yellow-container", {
+  keyframes: {
+    opacity: [0.5, 1, 0.3, 1],
+    // y: [0, 0, 100, 100, 0],
+    // ease: "power1.inOut"
+  },
+  duration: 0.2
+});
+
+
+
+tl.to(".ui-top-lvl2", {
+  keyframes: {
+    opacity: [0.7, 1, 0.9, 1],
+    // y: [0, 0, 100, 100, 0],
+    // ease: "power1.inOut"
+  },
+  duration: 0.2
+});
