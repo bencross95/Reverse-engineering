a51632a3bdba8bf6687b7bd75c5471505e4e3bd6
Ben Cross <ben_cross@me.com>
2024-02-02 17:18:23 +0100

    big fix attempt with anim

--- src/animations.js
+++ src/animations.js
@@ -1,29 +1,23 @@
-gsap.fromTo("body", { opacity: 0 }, { opacity: 1, duration: 1, delay: 0.2 });
+gsap.fromTo("body",
+ { opacity: 0 },
+ { opacity: 1, duration: 1, delay: 0.4 });
+
 
 var tl = gsap.timeline();
 
-// tl.to("summary", {
-//     keyframes: {
-//       opacity: [0, 0],
-//     },
-//     delay: 0,
-//     duration: 0.01,
-//     stagger: 0, // 0.1 seconds between when each ".box" element starts animating
-// });
-  
-function init () {
-tl.to("summary", {
-  keyframes: {
-    opacity: [0, 1],
-    autoAlpha: [0, 1]
-  },
-  delay: 0.2,
-  duration: 0.1,
-  ease: "power4.in",
-  stagger: 0.07, // 0.1 seconds between when each ".box" element starts animating
-});
-};
+function init() {
+  tl.to("summary", {
+    keyframes: {
+      opacity: [0, 1],
+      autoAlpha: [0, 1]
+    },
+    delay: 0.4,
+    duration: 0.1,
+    ease: "power4.in",
+    stagger: 0.07, // 0.1 seconds between when each ".box" element starts animating
+  });
+}
 
-window.addEventListener("load", function(event) {
+window.addEventListener("load", function (event) {
   init();
 });
